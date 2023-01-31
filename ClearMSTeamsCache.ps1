94acc9cfc8bf9a90ecf47685a3c60ccb8f2c09c9
djgerma <78435934+djgerma@users.noreply.github.com>
2023-01-31 09:23:57 -0500

    Add files via upload

---
+++ ClearMSTeamsCache.ps1
@@ -0,0 +1,42 @@
+﻿$clearCache = Read-Host "Do you want to delete the Teams Cache (Y/N)?"
+$clearCache = $clearCache.ToUpper()
+
+if ($clearCache -eq "Y"){
+  Write-Host "Closing Teams" -ForegroundColor Cyan
+  
+  try{
+    if (Get-Process -ProcessName Teams -ErrorAction SilentlyContinue) { 
+        Get-Process -ProcessName Teams | Stop-Process -Force
+        Start-Sleep -Seconds 5
+        Write-Host "Teams sucessfully closed" -ForegroundColor Green
+    }else{
+        Write-Host "Teams is already closed" -ForegroundColor Green
+    }
+  }catch{
+      echo $_
+  }
+
+  try {
+    if (Get-Process -ProcessName OUTLOOK -ErrorAction SilentlyContinue) {
+        Get-Process -ProcessName OUTLOOK | Stop-Process -Force
+        Start-Sleep -Seconds 5
+        Write-Host "Outlook sucessfully closed" -ForegroundColor Green
+    }else{
+        Write-Host "Outlook is already closed" -ForegroundColor Green
+    }
+   }catch{
+        echo $_
+    }
+
+  Write-Host "Clearing Teams cache" -ForegroundColor Cyan
+
+  try{
+    Get-ChildItem -Path $env:APPDATA\"Microsoft\teams" | Remove-Item -Recurse -Confirm:$false
+    Write-Host "Teams cache removed" -ForegroundColor Green
+  }catch{
+    echo $_
+  }
+
+  Write-Host "Cleanup complete... Launching Teams" -ForegroundColor Green
+  Start-Process -FilePath $env:LOCALAPPDATA\Microsoft\Teams\current\Teams.exe
+}
